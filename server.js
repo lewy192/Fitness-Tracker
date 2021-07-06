7c78899750041c28f4a0466b327517adc2b63a89
lewy192 <lewis.james.hill@outlook.com>
2021-07-06 14:10:31 +1000

    add: server.js working

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+require("dotenv").config();
+const router = require("./routes/index");
+
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(
+    `mongodb+srv://lewyRoot:${process.env.DB_PW}@coursecluster.q08bn.mongodb.net/Workout-Tracker?retryWrites=true&w=majority`,
+    { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: true }
+);
+
+app.use(router);
+
+app.listen(PORT, () => {
+    console.log(`App boot success on port: ${PORT}`);
+});
