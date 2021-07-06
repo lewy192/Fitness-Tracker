a3e8783f46de74352743ea6607f7bb937481a43c
lewy192 <lewis.james.hill@outlook.com>
2021-07-07 09:19:54 +1000

    update:schema options to allow virtuals

--- models/workout.js
+++ models/workout.js
@@ -2,46 +2,48 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const WorkoutSchema = new Schema({
-    date: { type: Date, default: Date.now },
-    exercises: [
-        {
-            name: {
-                required: "Please Enter a Name.",
-                trim: true,
-                type: String,
-            },
-            type: {
-                type: String,
-                trim: true,
-                required: " Please Enter Type of Exercise",
-            },
-            duration: {
-                type: Number,
-                required: "PleaseEnter Exercise duration",
-            },
-            weight: {
-                type: Number,
-            },
-            reps: {
-                type: Number,
-            },
-            sets: {
-                type: Number,
-            },
-            distance: {
-                type: Number,
-            },
-        },
-    ],
-});
+const schemaOptions = {
+    toJSON: {
+        virtuals: true,
+    },
+};
 
-// WorkoutSchema.methods.totalDuration = function () {
-//     return this.exercises.reduce(
-//         (accumulator, currentVal) => accumulator + currentVal.duration,
-//         0
-//     );
-// };
+const WorkoutSchema = new Schema(
+    {
+        date: { type: Date, default: Date.now },
+        exercises: [
+            {
+                name: {
+                    required: "Please Enter a Name.",
+                    trim: true,
+                    type: String,
+                },
+                type: {
+                    type: String,
+                    trim: true,
+                    required: " Please Enter Type of Exercise",
+                },
+                duration: {
+                    type: Number,
+                    required: "PleaseEnter Exercise duration",
+                },
+                weight: {
+                    type: Number,
+                },
+                reps: {
+                    type: Number,
+                },
+                sets: {
+                    type: Number,
+                },
+                distance: {
+                    type: Number,
+                },
+            },
+        ],
+    },
+    schemaOptions
+);
 
 let workoutSchemaVirtual = WorkoutSchema.virtual("totalDuration");
 
