5cb249a22c34b9b183597946f6825ec3860b37dc
lewy192 <lewis.james.hill@outlook.com>
2021-07-06 14:10:06 +1000

    add: route file structure

---
+++ routes/api/workout.js
@@ -0,0 +1,9 @@
+const router = require("express").Router();
+
+router.get("/", async (req, res) => {});
+// router.post();
+// router.put('/:id', async (req,res)=>{
+// });
+// router.delete();
+
+module.exports = router;
