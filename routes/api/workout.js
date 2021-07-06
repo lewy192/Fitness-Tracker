c6af7eee4698aa7fb49279c0bf5a467d897ac85a
lewy192 <lewis.james.hill@outlook.com>
2021-07-06 15:10:15 +1000

    add: get all workout route

--- routes/api/workout.js
+++ routes/api/workout.js
@@ -1,6 +1,19 @@
 const router = require("express").Router();
 
-router.get("/", async (req, res) => {});
+const Workout = require("../../models/workout");
+
+router.get("/", async (req, res) => {
+    try {
+        const workoutData = await Workout.find({}).sort({ date: "desc" });
+        if (!workoutData) {
+            res.status(200).json({ message: "No Workout data present" });
+        }
+        res.status(200).json(workoutData);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ errMessage: err });
+    }
+});
 // router.post();
 // router.put('/:id', async (req,res)=>{
 // });
