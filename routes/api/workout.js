6c563812c5d66789764739f270e6a9ec03ef9bd9
lewy192 <lewis.james.hill@outlook.com>
2021-07-07 10:18:28 +1000

    add:range route

--- routes/api/workout.js
+++ routes/api/workout.js
@@ -8,15 +8,56 @@ router.get("/", async (req, res) => {
         if (!workoutData) {
             res.status(200).json({ message: "No Workout data present" });
         }
+        // const workout = await new Workout(workoutData);
+        // console.log("workout:" + workout);
+        // workoutData.totalDuration = await workout.totalDuration();
+        // console.log(workoutData);
+        console.log(workoutData[workoutData.length - 1].totalDuration);
         res.status(200).json(workoutData);
     } catch (err) {
         console.log(err);
         res.status(500).json({ errMessage: err });
     }
 });
-// router.post();
-// router.put('/:id', async (req,res)=>{
-// });
-// router.delete();
+
+router.post("/", async (req, res) => {
+    try {
+        const { body: workoutBody } = req;
+        const newWorkout = await Workout.create(workoutBody);
+        console.log(newWorkout);
+        res.status(200).json(newWorkout);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ errMessage: err });
+    }
+});
+
+router.put("/:id", async (req, res) => {
+    try {
+        const { body: exerciseData } = req;
+        const { id: workoutId } = req.params;
+        const selectedWorkout = await Workout.findOneAndUpdate(
+            { _id: workoutId },
+            { $push: { exercises: exerciseData } }
+        );
+        res.status(200).json(selectedWorkout);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ errMessage: err });
+    }
+});
+
+router.get("/range", async (req, res) => {
+    try {
+        const rangedWorkoutData = await Workout.find({})
+            .sort({ date: "desc" })
+            .limit(7);
+
+        res.status(200).json(rangedWorkoutData);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ errMessage: err });
+    }
+});
 
 module.exports = router;
